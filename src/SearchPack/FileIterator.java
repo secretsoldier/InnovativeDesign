0e4077df438441e7f8a9b1d64115169839ac75f8
secret_survivor <c.mcbrayne@yahoo.co.uk>
2020-02-03 17:51:27 +0000

    Update Search API
    Create a seperate package for the Search related classes whilst also making FileIterator public for use outside of Search.

---
+++ src/SearchPack/FileIterator.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SearchPack;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author 18074751
+ */
+public class FileIterator implements Iterator {
+        private final boolean inDirectories;
+        private Queue<File> directories = new LinkedList(), subjects = new LinkedList();
+        
+        public FileIterator(File root, boolean inDirectories){
+            assert root.isDirectory();
+            directories.add(root);
+            this.inDirectories = inDirectories;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !(directories.isEmpty() && subjects.isEmpty());
+        }
+
+        @Override
+        public File next() {
+            if (subjects.isEmpty()){
+                for (File file : directories.poll().listFiles()){
+                    if (file.isDirectory() && this.inDirectories)
+                        directories.add(file);
+                    subjects.add(file);
+                }
+            }
+            return subjects.peek() == null ? next() : subjects.poll();
+        }
+        
+    }
--- src/SearchPack/Search.java
+++ src/SearchPack/Search.java
@@ -3,18 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package innovativedesign;
+package SearchPack;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 /**
  *
  * @author 18074751
  */
 public class Search {
     private final File root;
+    
     public Search(File file){
         this.root = file;
     }
@@ -28,8 +26,9 @@ public class Search {
         byDirect
     }
     private void fireSearchBegin(Search.SearchType type, String searchString){
-        for (ISearchListener e : listeners)
+        listeners.forEach((e) -> {
             e.SearchBegin(type, searchString);
+        });
     }
     private boolean fireSearchUpdate(File results){
         for (ISearchListener e : listeners)
@@ -38,43 +37,13 @@ public class Search {
         return false;
     }
     private void fireSearchEnd(ArrayList<File> results){
-        for (ISearchListener e : listeners)
+        listeners.forEach((e) -> {
             e.SearchEnd(results);
-    }
-    
-    private class FileIterator implements Iterator {
-        boolean inDirectories;
-        Queue<File> directories = new LinkedList();
-        Queue<File> subjects = new LinkedList();
-        
-        public FileIterator(boolean inDirectories){
-            assert root.isDirectory();
-            directories.add(root);
-            this.inDirectories = inDirectories;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return !(directories.isEmpty() && subjects.isEmpty());
-        }
-
-        @Override
-        public File next() {
-            if (subjects.isEmpty()){
-                for (File file : directories.poll().listFiles()){
-                    if (file.isDirectory() && this.inDirectories){
-                        directories.add(file);
-                    }
-                    subjects.add(file);
-                }
-            }
-            return subjects.poll();
-        }
-        
+        });
     }
     
     public ArrayList<File> byFile(String name, boolean inDirectories){
-        FileIterator files = new FileIterator(inDirectories);
+        FileIterator files = new FileIterator(root, inDirectories);
         ArrayList<File> results = new ArrayList();
         this.fireSearchBegin(SearchType.byFile, name); // Event start
         while (files.hasNext()){
@@ -92,7 +61,7 @@ public class Search {
     }
     
     public ArrayList<File> byDirectory(String name, boolean inDirectories){
-        FileIterator files = new FileIterator(inDirectories);
+        FileIterator files = new FileIterator(root, inDirectories);
         ArrayList<File> results = new ArrayList();
         this.fireSearchBegin(SearchType.byDirect, name); // Event start
         while (files.hasNext()){
