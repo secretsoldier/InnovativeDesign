4bc0aabd4f8fb3c2f233fab41df203d4323f85b3
CSAYO <47818825+CSAYO@users.noreply.github.com>
2020-03-09 16:54:11 +0000

    NMenuPush
    Create Nmenu for action listerns events

---
+++ src/ChartPack/NJMenu.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChartPack;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author 17007976
+ */
+public class NJMenu {
+    
+  public int addMenu(String name){
+      /*
+        JMenu newMenu = new JMenu(name);
+        newMenu.setVisible(true);
+        this.add(newMenu);
+        return this.getComponentCount() - 1;*/
+      return 0;
+    }
+}
